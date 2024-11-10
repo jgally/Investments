39ea37a68dc9129427ef11851a3a0b6d50928681
jgally <gallyjas@gmail.com>
2024-11-10 16:23:04 -0500

    Inputted other tabItem content so the 401k content would show up in correct tab. Removed history submenuitems bc unnecessary. Fixed indents

--- Investment-Dashboard/server.R
+++ Investment-Dashboard/server.R
@@ -15,5 +15,5 @@ server <- function(input, output, session) {
                
                
 
-})
+
 }
--- Investment-Dashboard/ui.R
+++ Investment-Dashboard/ui.R
@@ -14,37 +14,17 @@ shinyUI(fluidPage(
                     titleWidth = 250),
     #Sidebar options
     dashboardSidebar(
-      sidebarMenu(
+      sidebarMenu(id = "sidebarMenu",
         menuItem("401K",
-                 tabName = "traditional",
-                 startExpanded = TRUE,
-                 menuSubItem("History",
-                             tabName = "tradHist"),
-                 menuSubItem("Calculator",
-                             tabName = "tradCalc")
-                 ),
+                 tabName = "trad"),
         menuItem("Roth IRA",
-                 tabName = "ira",
-                 startExpanded = TRUE,
-                 menuSubItem("History",
-                             tabName = "iraHist"),
-                 menuSubItem("Calculator",
-                             tabName = "iraCalc")
-                 ),
+                 tabName = "ira"),
         menuItem("Stock Investments",
-                 tabName = "stocks",
-                 startExpanded = TRUE,
-                 menuSubItem("Historical Data",
-                             tabName = "stockHist"),
-                 menuSubItem("Investment Calculator",
-                             tabName = "stockCalc")
-                 )
-      )
-    ),
+                 tabName = "stocks")
+                 )),  
     #Here is the body content
     dashboardBody(
-      #Adding custom CSS to make title back
-      ground the same color as the rest of the header
+      #Adding custom CSS to make title background the same color as the rest of the header
       tags$head(tags$style(HTML('
         .skin-blue .main-header .logo {
           background-color: #3c8dbc;
@@ -53,55 +33,58 @@ shinyUI(fluidPage(
           background-color: #3c8dbc;
         }
                                 '))),
-      #Starting with the 401k content, debating on whether or not to have differing input options (typing in should probably be best)
-      tabItem("tradCalc",
-              fluidRow(
-                column(4,
-                       box(
-                title = "Let's Look at Your Numbers",
-                #current age
-                textInput("currentAge",
-                          label = "Current Age",
-                          value = "30",
-                          width = "100%"),
-                textOutput("age"),
-                #expected retirement age
-                numericInput("retireAge", 
-                            label = "Retiring Age",
-                            value = "65",
-                            min = 1,
-                            max = 120),
-                #current savings of 401k
-                numericInput("currentSavings"
-                             label = "Current Amount Invested",
-                             value = "30000"),
-                #present salary
-                numericInput("salary",
-                             label = "Your Salary",
-                             value = ""),
-                #salary increase
-                textInput("raises",
-                          label = "Estimated Yearly Raises",
-                          placeholder = "%"),
-                #monthly contribution
-                textInput("contributions",
-                          label = "Your 401k Contributions",
-                          placeholder = "10%"),
-                #employer match
-                textInput("eMatch",
-                          label = "Employer Match",
-                          placeholder = "5%"),
-                #estimated rate of return
-                numericInput("401kROI",
-                             label = "Expected Ra5te of Return on Investments",
-                             placeholder = "7%")
+      #Starting the tabItems
+      tabItems(
+        tabItem(tabName = "trad",
+                h2("Traditional 401K Calculator"),
+                  fluidRow(
+                    box(title = "Let's Look at Your Numbers",
+                      #current age
+                      textInput("currentAge",
+                                label = "Current Age",
+                                value = "30",
+                                width = "100%"),
+                      textOutput("age"),
+                      #expected retirement age
+                      numericInput("retireAge", 
+                                label = "Retiring Age",
+                                value = "65",
+                                min = 1,
+                                max = 120),
+                      #current savings of 401k
+                      numericInput("currentSavings",
+                                label = "Current Amount Invested",
+                                value = "30000"),
+                      #present salary
+                      numericInput("salary",
+                                label = "Your Salary",
+                                value = ""),
+                      #salary increase
+                      textInput("raises",
+                                label = "Estimated Yearly Raises",
+                                placeholder = "%"),
+                      #monthly contribution
+                      textInput("contributions",
+                                label = "Your 401k Contributions",
+                                placeholder = "10%"),
+                      #employer match
+                      textInput("eMatch",
+                                label = "Employer Match",
+                                placeholder = "5%"),
+                      #estimated rate of return
+                      numericInput("401kROI",
+                                label = "Expected Rate of Return on Investments",
+                                value = "7")
                 
-                
-              ),
-              column(8,
-                    plotOutput("401k",
-                              "Estimated 401k Investments")))  
-              ))
+              )  
+              )),
+        #Roth Ira tab
+        tabItem(tabName = "ira",
+              h2("Roth IRA Calculator")),
+        #Stock Calculator tab
+        tabItem(tabName = "stocks",
+              h2("Stock Investment Calculator"))
+      )
     )
-  )
 ))
+)
